007294939663f0deed209651fa546da63f8efcfb
MinhajpHero <154263291+MinhajpHero@users.noreply.github.com>
2024-04-15 10:51:24 +0600

    true vs. false

---
+++ true-false.js
@@ -0,0 +1,32 @@
+// https://github.com/MinhajpHero/advanced-javascript.git
+
+
+
+const age=4;
+
+if(age>0){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+//FALSE: 0, "", undefined, null, NaN 
+// TRUTH: '0', " "
+
+const name=""
+if(name){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+let something=" ";
+if(something){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+

525d4fd5dbddd0df55a640ed09ad57efac8db9da
MinhajpHero <154263291+MinhajpHero@users.noreply.github.com>
2024-04-16 10:19:18 +0600

    null vs undefined

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,22 +1,22 @@
 UNDEFINED
 
-1.let pakhi;
+let pakhi;
 console.log(pakhi);
 
-2.function add(num1,num2){
+function add(num1,num2){
     sum=num1+num2;
-return sum na dile undefined asbe
+// return sum na dile undefined asbe
 }
 const result=add(12,14);
 console.log(result);
 
-3.function add(num1,num2){
+function add(num1,num2){
     console.log(num1,num2);
 }
-const result=add(13);
+const result1=add(13);
 console.log(result);
 
-4.const lover={name:'smart dude',phone:5432344};
+const lover={name:'smart dude',phone:5432344};
 console.log(lover.gf);
 
 5.let fun=undefined;

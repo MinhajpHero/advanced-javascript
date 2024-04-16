459c7a812da0efc859f65dbf18563b0a2aa7342c
MinhajpHero <154263291+MinhajpHero@users.noreply.github.com>
2024-04-16 22:52:37 +0600

    object property

---
+++ object-property.js
@@ -0,0 +1,9 @@
+const students =[
+{id:21, name: 'omar sunny'},
+{id:31, name: 'Mannaaa'},
+{id:41, name: 'Dipjol'},
+];
+
+const names=students.map( s => s.name);
+const ids=students.map( s => s.id);
+console.log(ids);
